7c13a5a6834475916c30e0a600cefb8b816d6e3e
Vladislav Pogudin <bvlpo001@edu.xamk.fi>
2024-10-03 18:21:25 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+const path = require('path');
+const fs = require('fs'); // Подключаем модуль fs для работы с файлами
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: ['https://gudokvlad.netlify.app', 'http://localhost:3000'],
+    methods: ['GET', 'POST'],
+    credentials: true
+  }
+});
+
+app.use(cors({
+  origin: ['https://gudokvlad.netlify.app', 'http://localhost:3000'],
+  methods: 'GET,POST,PUT,DELETE',
+  credentials: true
+}));
+
+
+app.get("/activities", (req, res) => {
+    fs.readFile('./activities.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading activities.json:', err);
+        res.status(500).send('Internal Server Error');
+      } else {
+        const activities = JSON.parse(data);
+        res.json(activities);
+      }
+    });
+  });
+
+  app.get("/experiences", (req, res) => {
+    fs.readFile('./experiences.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading experiences.json:', err);
+        res.status(500).send('Internal Server Error');
+      } else {
+        const experiences = JSON.parse(data);
+        res.json(experiences);
+      }
+    });
+  });
+
+  app.get("/projects", (req, res) => {
+    fs.readFile('./projects.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading proejects.json:', err);
+        res.status(500).send('Internal Server Error');
+      } else {
+        const projects = JSON.parse(data);
+        res.json(projects);
+      }
+    });
+  });
+
+server.listen(5000, () => {
+  console.log("Server is running on port http://localhost:5000");
+});
